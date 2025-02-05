9289539ea99d4c8202f727fa75c35299a639d339
maria9753 <159113380+maria9753@users.noreply.github.com>
2025-02-05 23:45:49 +0100

    Create CarteleraCine.java

---
+++ Practica1/CarteleraCine.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class CarteleraCine {
+  private String nombreCine;
+  private List<Pelicula> peliculas = new ArrayList<>();
+  
+  public CarteleraCine(String cine, Pelicula[] peliculas) {
+    this.nombreCine = cine;
+    for (Pelicula p: peliculas)
+    this.peliculas.add(p);
+  }
+
+  public List<Pelicula> peliculasPorGenero(String genero) {
+    List<Pelicula> peliculasporgenero = new ArrayList<>();
+    for (Pelicula p: peliculas) {
+      if (p.getGenero() == genero) {
+        peliculasporgenero.add(p);
+      }
+    }
+    return peliculasporgenero;
+  }
+
+  public List<Pelicula> peliculasPosterioresA(int anyo) {
+    List<Pelicula> peliculasposterioresa = new ArrayList<>();
+    for (Pelicula p: peliculas) {
+      if (p.getAnyo() >= anyo) {
+        peliculasposterioresa.add(p);
+      }
+    }
+    return peliculasposterioresa;
+  }
+  
+  @Override
+  public String toString() {
+    return "Cine: "+this.nombreCine+"\nPeliculas en cartelera: "+this.peliculas;
+  }
+  
+  public static void main(String ...args) {
+    Pelicula peliculas[] = { new Pelicula("Perfect days", "Drama", 2023, "Wim Wenders"),
+                             new Pelicula("Inception", "Accion", 2010, "Christopher Nolan"),
+                             new Pelicula("Jumanji", "Aventura", 1995, "Joe Johnston") };
+    CarteleraCine cinesTelmo = new CarteleraCine("Telmo", peliculas);
+    System.out.println(cinesTelmo);
+    System.out.println("Dramas: "+cinesTelmo.peliculasPorGenero("Drama"));
+    System.out.println("Receintes: "+cinesTelmo.peliculasPosterioresA(2020));
+  }
+}
