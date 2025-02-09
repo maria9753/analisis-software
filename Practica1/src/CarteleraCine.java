42cff70e8f67fdd9b4761900b2ebec840e3508ce
CarmenGomezE <159149810+CarmenGomezE@users.noreply.github.com>
2025-02-09 13:16:48 +0100

    Add files via upload

--- Practica1/src/CarteleraCine.java
+++ Practica1/src/CarteleraCine.java
@@ -1,47 +1,90 @@
 import java.util.*;
 
+/**
+ * Esta aplicación representa una cartelera de cine y sus respectivas películas
+ * disponibles. Permite buscar películas por género o año de estreno
+ * 
+ * @author Carmen Gómez, María Pozo
+ */
 public class CarteleraCine {
-  private String nombreCine;
-  private List<Pelicula> peliculas = new ArrayList<>();
+	
+	/** Nombre del cine*/
+	private String nombreCine;
+	
+	/** Lista de películas del cine*/
+	private List<Pelicula> peliculas = new ArrayList<>();
   
-  public CarteleraCine(String cine, Pelicula[] peliculas) {
-    this.nombreCine = cine;
-    for (Pelicula p: peliculas)
-    this.peliculas.add(p);
-  }
+	/**
+	 * Constructor de la clase CarteleraCine
+	 * 
+	 * @param cine 			Nombre del cine
+	 * @param peliculas 	Array de las películas del cine
+	 */
+	public CarteleraCine(String cine, Pelicula[] peliculas) {
+		this.nombreCine = cine;
+		for (Pelicula p: peliculas)
+			this.peliculas.add(p);
+	}
 
-  public String peliculasPorGenero(String genero) {
-    List<Pelicula> peliculasporgenero = new ArrayList<>();
-    for (Pelicula p: peliculas) {
-      if (p.getGenero() == genero) {
-        peliculasporgenero.add(p);
-      }
-    }
-    return genero+": "+peliculasporgenero;
-  }
+	/**
+	 * Método para buscar películas por su género
+	 * 
+	 * @param genero 		El género de la película
+	 * @return Una cadena que contiene las películas cuyo género coincide con el 
+	 * recibido
+	 */
+	public String peliculasPorGenero(String genero) {
+		List<Pelicula> peliculasporgenero = new ArrayList<>();
+		for (Pelicula p: peliculas) {
+			if (p.getGenero().equals(genero)) {
+				peliculasporgenero.add(p);
+			}
+		}
+		return genero+": "+peliculasporgenero;
+	}
 
-  public String peliculasPosterioresA(int anyo) {
-    List<Pelicula> peliculasposterioresa = new ArrayList<>();
-    for (Pelicula p: peliculas) {
-      if (p.getAnyo() >= anyo) {
-        peliculasposterioresa.add(p);
-      }
-    }
-    return "Recientes:"+peliculasposterioresa;
-  }
+	/**
+	 * Método para buscar posteriores a un año dado
+	 * 
+	 * @param anyo 		El año de estreno de la película
+	 * @return Una cadena que contiene las películas estenadas después del año 
+	 * recibido
+	 */
+	public String peliculasPosterioresA(int anyo) {
+		List<Pelicula> peliculasposterioresa = new ArrayList<>();
+		for (Pelicula p: peliculas) {
+			if (p.getAnyo() >= anyo) {
+				peliculasposterioresa.add(p);
+			}
+		}
+		return "Recientes:"+peliculasposterioresa;
+	}
   
-  @Override
-  public String toString() {
-    return "Cine: "+this.nombreCine+"\nPeliculas en cartelera: "+this.peliculas;
-  }
+	/**
+	 * Método para generar las películas de un cine
+	 * 
+	 * @return Una cadena que contiene el cine y las películas que tiene en 
+	 * cartelera
+	 */
+	@Override
+	public String toString() {
+		return "Cine: "+this.nombreCine+"\nPeliculas en cartelera: "+this.peliculas;
+	}
   
-  public static void main(String ...args) {
-    Pelicula peliculas[] = { new Pelicula("Perfect days", "Drama", 2023, "Wim Wenders"),
-                             new Pelicula("Inception", "Accion", 2010, "Christopher Nolan"),
-                             new Pelicula("Jumanji", "Aventura", 1995, "Joe Johnston") };
-    CarteleraCine cinesTelmo = new CarteleraCine("Telmo", peliculas);
-    System.out.println(cinesTelmo);
-    System.out.println(cinesTelmo.peliculasPorGenero("Drama"));
-    System.out.println(cinesTelmo.peliculasPosterioresA(2020));
+	/**
+	 * Punto de entrada a la aplicación. 
+	 * Este método genera la cartelera del cine, imprime las películas cuyo género
+	 * es "Drama" y las películas estrenadas despúes del año 2020
+	 * 
+	 * @param args 		Los argumentos de la línea de comandos
+	 */
+	public static void main(String ...args) {
+		Pelicula peliculas[] = { new Pelicula("Perfect days", "Drama", 2023, "Wim Wenders"),
+                             	 new Pelicula("Inception", "Accion", 2010, "Christopher Nolan"),
+                             	 new Pelicula("Jumanji", "Aventura", 1995, "Joe Johnston") };
+		CarteleraCine cinesTelmo = new CarteleraCine("Telmo", peliculas);
+		System.out.println(cinesTelmo);
+		System.out.println(cinesTelmo.peliculasPorGenero("Drama"));
+		System.out.println(cinesTelmo.peliculasPosterioresA(2020));
   }
 }
--- Practica1/src/Pelicula.java
+++ Practica1/src/Pelicula.java
@@ -1,35 +1,73 @@
-
+/**
+ * Representa películas que contienen título, género, año y director.
+ * Esta clase se utiliza en CarteleraCine
+ * 
+ * @author Carmen Gómez, María Pozo
+ */
 public class Pelicula {
-  private String titulo;
-  private String genero;
-  private int anyo;
-  private String director;
+	private String titulo;
+	private String genero;
+	private int anyo;
+	private String director;
   
-  public Pelicula (String titulo, String genero, int anyo, String director) {
-    this.titulo = titulo;
-    this.genero = genero;
-    this.anyo = anyo;
-    this.director = director;
-  }
+	/**
+	 * Constructor de la clase Pelicula
+	 * 
+	 * @param titulo 			Nombre de la película
+	 * @param genero 			Género de la película
+	 * @param anyo 				Año de estreno de la película
+	 * @param director 			Director de la película
+	 */
+	public Pelicula (String titulo, String genero, int anyo, String director) {
+		this.titulo = titulo;
+		this.genero = genero;
+		this.anyo = anyo;
+		this.director = director;
+	}
   
-  @Override
-  public String toString() {
-    return this.titulo+" - dirigida por: "+this.director+" ("+this.genero+"): "+this.anyo;
-  }
+	/**
+	 * Método para generar cadenas de las películas con sus respectivos atributos
+	 * 
+	 * @return Una cadena que contiene el título, gñenero, anyo y director de la película
+	 */
+	@Override
+	public String toString() {
+		return this.titulo+" - dirigida por: "+this.director+" ("+this.genero+"): "+this.anyo;
+	}
   
-  public String getTitulo() {
-    return titulo;
-  }
+	/**
+	 * Método para obtener el título de una película
+	 * 
+	 * @return Una cadena con el título de la película
+	 */
+	public String getTitulo() {
+		return titulo;
+	}
   
-  public String getGenero() {
-    return genero;
-  }
+	/**
+	 * Método para obtener el género de una película
+	 * 
+	 * @return Una cadena con el género de la película
+	 */
+	public String getGenero() {
+		return genero;
+	}
   
-  public int getAnyo() {
-    return anyo;
-  }
-
-  public String getDirector() {
-    return director;
-  }
+	/**
+	 * Método para obtener el anyo de una película
+	 * 
+	 * @return Una entero que representa el año de estreno de la película
+	 */
+	public int getAnyo() {
+		return anyo;
+	}
+	
+	/**
+	 * Método para obtener el director de una película
+	 * 
+	 * @return Una cadena con el director de la película
+	 */
+	public String getDirector() {
+		return director;
+	}
 }
