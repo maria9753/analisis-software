6682707b429b96ecea6cbbc4926cf8fa1ab0e2cf
CarmenGomezE <carmengomezescobarr@gmail.com>
2025-02-12 18:22:22 +0100

    changes

--- Practica2/BookStore.java
+++ Practica2/BookStore.java
@@ -1,13 +1,16 @@
 package orders;
 public enum BookStore {
-MAIN("Fifth Avenue. 73, Manhattan, NYC"),
-BROOKLYN("Bedford Avenue 24, Brooklyn, NYC"),
-NEWARK("Broad Street 11, Newark, NJ");
-private String address;
-private BookStore(String address) {
-this.address = address;
-}
-public String toString() {
-return this.address;
-}
+    MAIN("Fifth Avenue. 73, Manhattan, NYC"),
+    BROOKLYN("Bedford Avenue 24, Brooklyn, NYC"),
+    NEWARK("Broad Street 11, Newark, NJ");
+    
+    private String address;
+
+    private BookStore(String address) {
+        this.address = address;
+    }
+    
+    public String toString() {
+        return this.address;
+    }
 }
--- Practica2/InStoreOrder.java
+++ Practica2/InStoreOrder.java
@@ -1,12 +1,14 @@
 package orders;
 public class InStoreOrder extends Order {
-private BookStore store;
-public InStoreOrder(String item, double price, String customer, BookStore store){
-super(item, price, customer);
-this.store = store;
-}
-@Override
-public String toString() {
-return super.toString()+"\nStore: "+this.store;
-}
+    private BookStore store;
+    
+    public InStoreOrder(String item, double price, String customer, BookStore store){
+        super(item, price, customer);
+            this.store = store;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"\nStore: "+this.store;
+    }
 }
