a2deff266493a98be56e9b1a3ef8390d31452add
CarmenGomezE <carmengomezescobarr@gmail.com>
2025-02-19 18:26:48 +0100

    changes

--- Practica2/apartado1/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"orders","l":"BookStore"},{"p":"orders","l":"InStoreOrder"},{"p":"orders","l":"Main"},{"p":"orders","l":"OnlineOrder"},{"p":"orders","l":"Order"}];updateSearchResults();
