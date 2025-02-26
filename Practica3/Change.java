3ca372fac79c4c9cf7bfff9880835dd32c003acc
maria9753 <159113380+maria9753@users.noreply.github.com>
2025-02-26 18:41:20 +0100

    Update Change.java

---
+++ Practica3/Change.java
@@ -0,0 +1,35 @@
+
+package changes;
+/**
+ * La clase Change representa los cambios. 
+ * 
+ * @author Carmen Gómez, María Pozo.
+ */
+public abstract class Change {
+    /** Ruta del fichero*/
+    private String path;
+    /** Número de línea inicial*/
+    private int startLine;
+    /** Tipo de cambio*/
+    private enum 
+
+    /**
+	 * Constructor de la clase Change.
+	 * 
+	 * @param path 			Ruta del fichero.
+	 * @param price 	  Número de línea incial.
+	 */
+    public Change(String path, int startLine) {
+        this.path = path;
+        this.startLine = startLine;
+    }
+    
+    /**
+	 * Método para generar cambios.
+	 * 
+	 * @return Una cadena que contiene los detalles del cambio.
+	 */
+    public String toString() {
+        return "Order of '"+this.item+"' for "+this.customer+" ("+this.totalPrice()+"$)";
+    }
+}
