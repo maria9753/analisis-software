f01d1b07f3a85269145eb18f132bf27544a3d6f9
CarmenGomezE <carmengomezescobarr@gmail.com>
2025-04-15 11:21:33 +0200

    apartado a

--- Practica5/NumericData.java
+++ Practica5/NumericData.java
@@ -1,3 +1,14 @@
-public class NumericData {
+import java.util.LinkedHashMap;
 
+public class NumericData extends LinkedHashMap<String, Integer> {
+    public NumericData(int op1, int op2) {
+        this.put("op1", op1);
+        this.put("op2", op2);
+        this.put("result", 0);
+    }
+
+    @Override
+    public String toString() {
+        return "{op1=" + get("op1") + ", op2=" + get("op2") + ", result=" + get("result") + "}";
+    }
 }
--- Practica5/StateGraph.java
+++ Practica5/StateGraph.java
@@ -1,3 +1,75 @@
-public class StateGraph {
+import java.util.*;
+import java.util.function.Consumer;
 
+public class StateGraph<T> {
+    private final String name;
+    private final String description;
+    private final Map<String, Consumer<T>> nodes = new LinkedHashMap<>();
+    private final Map<String, List<String>> edges = new HashMap<>();
+    private final Set<String> finalNodes = new HashSet<>();
+    private String initialNode;
+
+    public StateGraph(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public StateGraph<T> addNode(String nodeName, Consumer<T> action) {
+        nodes.put(nodeName, action);
+        return this;
+    }
+
+    public void addEdge(String from, String to) {
+        edges.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+    }
+
+    public void setInitial(String nodeName) {
+        this.initialNode = nodeName;
+    }
+
+    public void setFinal(String nodeName) {
+        finalNodes.add(nodeName);
+    }
+
+    public T run(T input, boolean debug) {
+        Set<String> visited = new HashSet<>();
+        Deque<String> stack = new ArrayDeque<>();
+        stack.push(initialNode);
+        int step = 1;
+
+        if (debug) {
+            System.out.println("- Initial: " + initialNode);
+            System.out.println("- Final: " + finalNodes);
+            System.out.println("input = " + input);
+        }
+
+        while (!stack.isEmpty()) {
+            String current = stack.pop();
+            if (visited.contains(current)) continue;
+            visited.add(current);
+
+            Consumer<T> action = nodes.get(current);
+            if (action != null) {
+                if (debug) {
+                    System.out.println("Step " + (step++) + " (" + name + ") - " + current + " executed: " + input);
+                }
+                action.accept(input);
+            }
+
+            List<String> next = edges.getOrDefault(current, new ArrayList<>());
+            for (int i = next.size() - 1; i >= 0; i--) {
+                stack.push(next.get(i));
+            }
+        }
+
+        return input;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Workflow '").append(name).append("' (").append(description).append("):\n");
+        sb.append("- Nodes: ").append(nodes.keySet()).append("\n");
+        return sb.toString();
+    }
 }
